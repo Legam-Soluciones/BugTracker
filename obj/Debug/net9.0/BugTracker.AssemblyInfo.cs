afb32471b3cc0e6fee6d4058595dd12e82fec5dd
Luis Enrique <le.gamboa@gmail.com>
2025-03-15 21:04:23 -0600

    commit 1f0d5c83006b64c9176066218a712635604ebf55 Author: Legam <legam@legam.com> Date:   Fri Mar 15 20:37:35 2023 +0100
        fix-post-request-v1.2   Arreglo de Ticket y Comment ajustes en el codigo fuente

--- obj/Debug/net9.0/BugTracker.AssemblyInfo.cs
+++ obj/Debug/net9.0/BugTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BugTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48e4ac184bd5814b33dbe0597b0b4cc26f4af004")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0d5c83006b64c9176066218a712635604ebf555")]
 [assembly: System.Reflection.AssemblyProductAttribute("BugTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BugTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
